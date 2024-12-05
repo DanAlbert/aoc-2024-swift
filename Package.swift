3fc65977e22016c2f87143120b7211344788d8bc
Dan Albert <danalbert@google.com>
2024-12-04 16:04:40 -0800

    Add base Swift project.

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Advent2024",
+    targets: [
+        .executableTarget(
+            name: "Day1"),
+    ]
+)
